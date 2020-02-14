04d177bf500a22568ca5a39deedfc9e1f3096446
shindeswapnil <60998799+shindeswapnil@users.noreply.github.com>
2020-02-14 18:17:56 +0530

    reverse()
    Define a function reverse() that computes the reversal of a string. For example, reverse("I am testing") should return the string "gnitset ma I"

---
+++ 7.py
@@ -0,0 +1,9 @@
+#Define a function reverse() that computes the reversal of a string. For example, reverse("I am testing") should return the string "gnitset ma I"
+
+def my_rev(s):
+	s1=s[::-1]
+	return s1
+
+print(my_rev('I am testing'))
+
+
